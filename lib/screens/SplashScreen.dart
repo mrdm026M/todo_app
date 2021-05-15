39a585822698e92ce478e02ea1d8393a6ce63f5a
Dhruv Maheshwari <mrdm026@gmail.com>
2021-05-16 03:25:53 +0530

    splash screen completed

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:todo_app/data/Colors.dart';
 import 'package:todo_app/screens/HomeScreen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -14,16 +15,54 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-        Duration(seconds: 10),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => HomeScreen())));
+      Duration(seconds: 6),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomeScreen(),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.red,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'TODO',
+              style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 60.0,
+                  fontWeight: FontWeight.w800,
+                  color: AppColors.TextColor,
+                  letterSpacing: 2,
+                  height: 0.5),
+            ),
+            Text(
+              'APP',
+              textAlign: TextAlign.right,
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 30.0,
+                fontWeight: FontWeight.w500,
+                color: AppColors.TextColor,
+              ),
+            ),
+          ],
+        ),
+        decoration: BoxDecoration(
+          color: AppColors.BgColor,
+          image: DecorationImage(
+            image: new AssetImage("assets/images/splashscreen.jpg"),
+            fit: BoxFit.fill,
+          ),
+        ),
       ),
     );
   }
