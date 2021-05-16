149dccb2bae8283c65b27fb17b2e7ca6ded88e66
Dhruv Maheshwari <mrdm026@gmail.com>
2021-05-16 18:36:12 +0530

    added some new colors

--- lib/data/Colors.dart
+++ lib/data/Colors.dart
@@ -5,7 +5,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const TextColor = Color(0xFFEEEEEE);
   static const BgColor = Color(0xFF393E46);
-  static const SubTextColor = Color(0xFFA2B29F);
+  static const SubBgColor = Color(0xFF464E57);
+  static const SubTextColor = Color(0xFFAFAFAF);
   static const TaskColor = Color(0xFF00ADB5);
   static const CompleteTaskColor = Color(0xFFAAD8D3);
 }
